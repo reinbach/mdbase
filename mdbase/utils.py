db6c5d3151b51686a8b73fa4d8e8c59ac1f3858f
Greg Reinbach <greg@reinbach.com>
2013-04-28 18:49:09 -0400

    added rest of unit tests for utils and cleaned up utils slightly

--- mdbase/utils.py
+++ mdbase/utils.py
@@ -5,25 +5,12 @@ import os
 
 import zmq
 
-def dump(msg_or_socket):
+def dump(msg):
     """Received all message parts from socket, prints neatly"""
-    if isinstance(msg_or_socket, zmq.Socket):
-        # it's a socket, call on current message
-        return dump(msg_or_socket.recv_multipart())
-    else:
-        msg = msg_or_socket
-
     print("-" * 40)
     for part in msg:
         print("[%03d]" % len(part)),
         print(part)
-
-def socket_set_hwm(socket, hwm=-1):
-    """libzmq 2/3 compatible set hwm"""
-    try:
-        socket.sendhwm = socket.rcvhwm = hwm
-    except AttributeError:
-        socket.hwm = hwm
 
 def zpipe(ctx):
     """Build inproc pipe for talking to threads
@@ -34,10 +21,10 @@ def zpipe(ctx):
     """
     a = ctx.socket(zmq.PAIR)
     a.linger = 0
+    a.set_hwm(1)
     b = ctx.socket(zmq.PAIR)
     b.linger = 0
-    socket_set_hwm(a, 1)
-    socket_set_hwm(b, 1)
+    b.set_hwm(1)
     iface = "inproc://%s" % binascii.hexlify(os.urandom(8))
     a.bind(iface)
     b.connect(iface)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,19 @@
 import unittest
+import zmq
 
 from test import support
 
-from mdbase.utils import dump
+from mdbase.utils import dump, zpipe
 
 BUFFER = "-" * 40
 
 class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.ctx = zmq.Context()
+
+    def tearDown(self):
+        self.ctx.destroy()
+
     def test_dump(self):
         """Test dump function"""
         msg = ['hello']
@@ -33,6 +40,27 @@ class TestUtils(unittest.TestCase):
             "%s\n%s\n%s\n" % (BUFFER, "[001]", msg[0]),
             s.getvalue()
         )
+
+    def test_zpipe(self):
+        """Test zpipe method
+
+        Setting up two inproc PAIRs for threading
+        """
+        s1, s2 = zpipe(self.ctx)
+
+        # check high water mark setting
+        self.assertEqual(s1.get_hwm(), 1)
+        self.assertEqual(s2.get_hwm(), 1)
+
+        # check linger setting
+        self.assertEqual(s1.get(zmq.LINGER), 0)
+        self.assertEqual(s2.get(zmq.LINGER), 0)
+
+        # check sending message between pair
+        msg = [b'', b'hello']
+        s1.send_multipart(msg)
+        resp = s2.recv_multipart()
+        self.assertEqual(msg, resp)
 
 if __name__ == "__main__":
     unittest.main()
