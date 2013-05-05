2ee344fd1dcc94d1151f3f067dd451b2f8085947
Greg Reinbach <greg@reinbach.com>
2013-05-05 08:22:37 -0400

    added a couple of unittests to worker, skipped for now

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,6 +1,8 @@
 import unittest
 import zmq
 
+from mdbase import constants
+from mdbase.broker import MajorDomoBroker
 from mdbase.worker import MajorDomoWorker
 
 
@@ -19,3 +21,17 @@ class TestMajorDomoWorker(unittest.TestCase):
         self.assertEqual(w.verbose, verbose)
         self.assertIsInstance(w.ctx, zmq.Context)
         self.assertIsInstance(w.poller, zmq.Poller)
+
+    @unittest.skip("determine how to mock the necessary parts")
+    def test_send_to_broker_model(self):
+        """Test send message to broker"""
+        b = MajorDomoBroker(False)
+        w = MajorDomoWorker(self.broker_url, b"echo", False)
+        w.send_to_broker(constants.W_REQUEST, b.service, [b"test"])
+
+    @unittest.skip("need to test send to broker method first")
+    def test_reconnect_to_broker_model(self):
+        """Test reconnecting to broker"""
+        b = MajorDomoBroker(False)
+        w = MajorDomoWorker(self.broker_url, b"echo", False)
+        
